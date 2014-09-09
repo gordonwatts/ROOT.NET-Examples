49091956f3fb8683ca1080027adef0bd18ab88d9
Gordon Watts <gordon@gordonandpaula.net>
2014-09-10 00:30:58 +0200

    Really simple dynamic object creation

---
+++ DynamicObjectCreation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicObjectCreation
+{
+    /// <summary>
+    /// Create an object dynamically. Note that this program has a dependence ONLY on the
+    /// dynamic ROOT.NET library. This means pretty much the only wrapper in here are some basic
+    /// TObject like objects. Everything else doesn't have a wrapper - like TObjString.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = ((dynamic)ROOTNET.Utility.ROOTCreator.ROOT).TObjString("dude");
+            s.Print();
+            // Prints out "dude".
+        }
+    }
+}
