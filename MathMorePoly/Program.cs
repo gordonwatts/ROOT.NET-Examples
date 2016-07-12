6cfab8abb00aee8f196cad1e6e935246aac4e700
Gordon Watts <gordon@gordonandpaula.net>
2016-07-12 01:42:09 -0700

    Add code to go after the poly function

--- MathMorePoly/Program.cs
+++ MathMorePoly/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace MathMorePoly
 {
     /// <summary>
@@ -13,6 +15,12 @@ namespace MathMorePoly
     {
         static void Main(string[] args)
         {
+            // 1.0*x + 5.0
+            var p = new ROOTNET.NROOT.NMath.NPolynomial(1.0, 5.0);
+            double value = 0.0, derivative = 0.0;
+            p.FdF(2.0, ref value, ref derivative);
+
+            WriteLine($"Value = {value} (should be 7), derivative is {derivative} (should be 1).");
 
         }
     }
