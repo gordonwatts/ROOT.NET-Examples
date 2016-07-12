909703de6f7d368c757056217329a0a111072037
Gordon Watts <gordon@gordonandpaula.net>
2016-07-12 01:07:28 -0700

    Add a shell for the next check code/example.

---
+++ MathMorePoly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMorePoly
+{
+    /// <summary>
+    /// Make sure we got the math more polynomial class
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
